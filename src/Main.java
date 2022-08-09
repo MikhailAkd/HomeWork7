7d20948bfc2e0aa36ee0b444cb18f6bd67b36459
MikhailAkd <mihailbuspoi@gmail.com>
2022-08-09 09:39:34 +0300

    Выполнил домашнюю работу 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        //тут будет код :)
+
+        //Задача 1
+
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
+
+        //Задача 2
+
+        String fullNameCaps = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameCaps);
+        System.out.println();
+
+        //Задача 3
+
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
